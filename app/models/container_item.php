18408931d3b5dcc295cc693437a1b0c540a8b39e
Chris Saylor <cjsaylor@gmail.com>
2011-03-02 23:30:25 -0500

    Added the actual models.  Modified user model to link with container.

---
+++ app/models/container_item.php
@@ -0,0 +1,47 @@
+<?php
+class ContainerItem extends AppModel {
+	var $name = 'ContainerItem';
+	var $displayField = 'title';
+	var $validate = array(
+		'container_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'category_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	var $belongsTo = array(
+		'Container' => array(
+			'className' => 'Container',
+			'foreignKey' => 'container_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => 'Container.created',
+                        'counterCache' => true
+		),
+		'Category' => array(
+			'className' => 'Category',
+			'foreignKey' => 'category_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
+?>
