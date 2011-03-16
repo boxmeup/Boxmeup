4740ee5520e90e446fbbd2f7b7c29a6cfd312631
Chris Saylor <cjsaylor@gmail.com>
2011-03-15 23:08:35 -0400

    Added container item addition.  Updated dashboard for simple statistics.  Added ability to delete container (which also deletes dependent container items).

--- app/controllers/containers_controller.php
+++ app/controllers/containers_controller.php
@@ -1,4 +1,5 @@
 <?php
+App::import('lib', array('Sanitize'));
 class ContainersController extends AppController {
 
 	public $name = 'Containers';
@@ -16,6 +17,9 @@ class ContainersController extends AppController {
 	}
 
 	public function dashboard() {
+		$total_containers = $this->Container->getTotalContainersPerUser($this->Auth->user('id'));
+		$total_container_items = $this->Container->getTotalContainerItemsPerUser($this->Auth->user('id'));
+		$this->set(compact('total_containers', 'total_container_items'));
 		$this->set('active', 'containers.dashboard');
 	}
 
@@ -55,12 +59,26 @@ class ContainersController extends AppController {
 			if($results) {
 				$this->Session->setFlash('Successfully added new container', 'notification/success');
 				$page = (int) ceil($this->Container->getTotalContainersPerUser($this->Session->read('Auth.User.id')) / $this->_container_page_limit);
-				$this->redirect(array('controller' => 'containers', 'action' => 'index', 'page' => $page));
+				$this->redirect(array('controller' => 'containers', 'action' => 'view', $results['Container']['slug']));
 			} else {
 				$this->Session->setFlash('There was a problem saving your container.', 'notification/error');
 			}
 		}
 		$this->set('control', 'containers.add');
+	}
+
+	public function delete($container_uuid) {
+		if(empty($container_uuid)) {
+			$this->Session->setFlash(__('Invalid container ID.', true), 'notification/error');
+			$this->redirect(array('controller' => 'containers', 'action' => 'index'));
+		} else {
+
+			if($this->Container->delete($this->Container->getIdByUUID($container_uuid)))
+				$this->Session->setFlash(__('Successfully deleted container and all contained items.', true), 'notification/success');
+			else
+				$this->Session->setFlash(__('Error deleting container.', true), 'notification/error');
+			$this->redirect(array('controllers' => 'containers', 'action' => 'index'));
+		}
 	}
 
 	private function verifyUser($id) {
