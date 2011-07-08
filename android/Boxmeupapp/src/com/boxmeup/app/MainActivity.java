fd6ffd66b87a875365dca277acd2cf955b7827f6
Chris Saylor <cjsaylor@gmail.com>
2011-07-08 19:05:08 -0400

    Updated codebase to be complient with android market.

--- android/Boxmeupapp/src/com/boxmeup/app/MainActivity.java
+++ android/Boxmeupapp/src/com/boxmeup/app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.boxmeup.app;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -9,6 +11,9 @@ import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends Activity {
 	
@@ -26,6 +31,9 @@ public class MainActivity extends Activity {
 		mWebView.getSettings().setJavaScriptEnabled(true);
 		mWebView.loadUrl(getString(R.string.url_source));
 		mWebView.setWebViewClient(new BoxmeupWebViewClient());
+		
+		// Initialize the javascript interface
+		mWebView.addJavascriptInterface(new BoxmeupJavascriptInterface(this), getString(R.string.javascript_interface));
 	}
 	
 	@Override
@@ -35,6 +43,14 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if (scanResult != null) {
+			mWebView.loadUrl(scanResult.getContents());
+		}
 	}
 	
 	/**
@@ -77,6 +93,27 @@ public class MainActivity extends Activity {
 		public void onPageFinished(WebView view, String url) {
 			mImageView.setVisibility(View.GONE);
 			view.setVisibility(View.VISIBLE);
+		}
+	}
+	
+	/**
+	 * Javascript interface that allows the web application to talk to the
+	 * android app.
+	 */
+	private class BoxmeupJavascriptInterface {
+		Context mContext;
+		
+		BoxmeupJavascriptInterface(Context c) {
+			mContext = c;
+		}
+		
+		/** Show a toast from the web page */
+		public void showToast(String toast) {
+			Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+		}
+		
+		public void qrScan() {
+			IntentIntegrator.initiateScan((Activity) mContext);
 		}
 	}
 }
