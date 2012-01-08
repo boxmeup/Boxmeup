4e642537049e1f2f1b6fe90f5df0fa8c5c33cf81
Chris Saylor <cjsaylor@gmail.com>
2012-01-08 01:02:17 -0500

    Updated api container resource with search method.  Moved sphinx search query into model and updated search controller to use model method.

--- app/controllers/searches_controller.php
+++ app/controllers/searches_controller.php
@@ -33,19 +33,7 @@ class SearchesController extends AppController {
 			}
 		} else {
 			$this->data['Search']['query'] = $query;
-	
-			$this->paginate = array(
-				'search' => urldecode($query),
-				'sphinx' => array(
-					'matchMode' => SPH_MATCH_ALL,
-					'index' => array('container_items', 'container_items_delta'),
-					'sortMode' => array(SPH_SORT_TIME_SEGMENTS => 'created'),
-				),
-				'contain' => array('Container')
-			);
-	
-			$this->paginate['sphinx']['filter'][] = array('user_id', $this->Auth->user('id'));
-			$this->set('results', $this->paginate('ContainerItem'));
+			$this->set('results', $this->ContainerItem->searchContainers($this, $this->Auth->user('id'), urldecode($query)));
 		}
 		
 		$this->set('active', 'container_items.index');
--- app/plugins/api/controllers/containers_controller.php
+++ app/plugins/api/controllers/containers_controller.php
@@ -71,4 +71,36 @@ class ContainersController extends ApiAppController {
 			return false;
 		}
 	}
+
+	public function search() {
+		if(!$this->RequestHandler->isGet()) {
+			$this->setError(405, 'Resource method supports GET only.');
+			return false;
+		}
+		if(empty($this->params['url']['query'])) {
+			$this->setError(406, "You must specify a search query.");
+			return false;
+		}
+		$this->params['named']['page'] = !empty($this->params['url']['page']) ? $this->params['url']['page'] : 1;
+		$results = $this->Container->ContainerItem->searchContainers($this, $this->user_id, $this->params['url']['query']);
+		if(empty($results)) {
+			$this->setError(404, 'No results found.');
+			return false;
+		}
+		// Format results
+		foreach($results as $key => $result) {
+			unset(
+				$results[$key]['ContainerItem']['id'],
+				$results[$key]['ContainerItem']['container_id'],
+				$results[$key]['Container']['id'],
+				$results[$key]['Container']['user_id'],
+				$results[$key]['Container']['location_id']
+			);
+		}
+		$this->output = array(
+			'search' => $results,
+			'pages' => $this->params['paging']['ContainerItem']['pageCount'],
+			'total' => $this->params['paging']['ContainerItem']['count']
+		);
+	}
 }
