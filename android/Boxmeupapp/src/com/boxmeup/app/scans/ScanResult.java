a2b0c0c1827c740d8e29291268866b4d2c33846d
Chris Saylor <cjsaylor@gmail.com>
2012-04-12 07:18:03 -0400

    Moved android app to it's own repo.

--- android/Boxmeupapp/src/com/boxmeup/app/scans/ScanResult.java
+++
@@ -1,8 +0,0 @@
-package com.boxmeup.app.scans;
-
-import android.content.Context;
-import com.google.zxing.integration.android.IntentResult;
-
-public interface ScanResult {
-	public boolean processResult(IntentResult scanResult);
-}
