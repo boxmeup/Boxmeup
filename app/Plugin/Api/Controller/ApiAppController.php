b54ecab0991a79436f7f0c2fec911df98fb1a406
Chris Saylor <cjsaylor@gmail.com>
2012-10-08 23:38:43 -0400

    Further API changes for containers.

--- app/Plugin/Api/Controller/ApiAppController.php
+++ app/Plugin/Api/Controller/ApiAppController.php
@@ -8,8 +8,17 @@ class ApiAppController extends AppController {
 
 	public $components = array('Api.ApiAuth');
 
+	protected $userId;
+
 	public function jsonOutput($data) {
 		$this->set(array('data' => $data, '_serialize' => 'data'));
+	}
+
+	public function getUserId() {
+		if (empty($this->userId)) {
+			$this->userId = $this->ApiUser->getUserId($this->request->data['ApiUser']['api_key']);
+		}
+		return $this->userId;
 	}
 
 }
