ec2a618ff24fa03d5a1b8dc2114f8814853be152
Chris Saylor <cjsaylor@gmail.com>
2013-03-10 20:43:25 -0400

    Added session flash messages to mobile layout.
    boxmeup/Boxmeup#136

--- app/webroot/js/app.mobile.js
+++ app/webroot/js/app.mobile.js
@@ -27,4 +27,7 @@ $(document).on('pageload', function() {
 $(document).ready(function(){
 	// If we are contained within the android app, attach the QR scanning
 	mobileApp.initAndroid();
+	$('.alert-message').on('click', function() {
+		$(this).slideUp();
+	})
 });
