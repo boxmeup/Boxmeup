023d65751ae5bb4d7342345b42189b167fc1b400
Chris Saylor <cjsaylor@gmail.com>
2014-07-23 18:38:14 -0400

    Added a simplistic static configuration reader.

---
+++ src/Util/Configure.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Boxmeup\Web\Util;
+
+class Configure {
+
+	public static function get($path) {
+		$loc = static::retrieveConfig();
+		foreach (explode('.', $path) as $step) {
+			$loc = $loc[$step];
+		}
+		return $loc;
+	}
+
+	protected static function retrieveConfig() {
+		static $cache = null;
+
+		if ($cache === null) {
+			$cache = require __DIR__ . '/../../config/environment.php';
+		}
+
+		return $cache;
+	}
+
+}
