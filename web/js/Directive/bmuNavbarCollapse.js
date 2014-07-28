f7373f5b6cd8c3e7957f18c61d06e71379a05ca3
Chris Saylor <cjsaylor@gmail.com>
2014-07-28 11:37:58 -0400

    Small efficiency change in navbar collapse.

--- web/js/Directive/bmuNavbarCollapse.js
+++ web/js/Directive/bmuNavbarCollapse.js
@@ -3,9 +3,8 @@ define(['angular'], function(angular) {
 	return function() {
 		return {
 			link: function(scope, el) {
-				var toggle;
+				var toggle = el.find('.navbar-toggle');
 				el.find('.navbar-collapse a').on('click', function() {
-					toggle = el.find('.navbar-toggle');
 					if (toggle.length && toggle.css('display') !== 'none') {
 						toggle.trigger('click');
 					}
