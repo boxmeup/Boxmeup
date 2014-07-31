12fabf215849cec1d786302866032bd834029976
Chris Saylor <cjsaylor@gmail.com>
2014-07-31 17:33:05 -0400

    Added a container save endpoint.
    /app/containers/save/

--- src/Controller/ContainerController.php
+++ src/Controller/ContainerController.php
@@ -4,7 +4,9 @@ namespace Boxmeup\Web\Controller;
 
 use Boxmeup\Web\Base\ControllerInterface;
 use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Boxmeup\Web\Base\Application;
+use Boxmeup\Container\Container;
 
 class ContainerController implements ControllerInterface, ControllerProviderInterface
 {
@@ -17,6 +19,7 @@ class ContainerController implements ControllerInterface, ControllerProviderInte
 	public function connect(\Silex\Application $app) {
 		$controllers = $app['controllers_factory'];
 		$controllers->get('/', 'controller.container:index');
+		$controllers->post('/save/', 'controller.container:save');
 
 		return $controllers;
 	}
@@ -27,6 +30,21 @@ class ContainerController implements ControllerInterface, ControllerProviderInte
 			'containers' => $results['data']->toArray(),
 			'total' => $results['total']
 		]);
+	}
+
+	public function save(Request $request) {
+		if (!is_string($request->getContent())) {
+			throw new \InvalidArgumentException();
+		}
+		$container = new Container(json_decode($request->getContent(), true));
+		$container['user'] = $this->app->user();
+		try {
+			$this->app['repo.container']->save($container);
+		} catch (\LogicException $e) {
+			return $this->app->json(['message' => $e->getMessage()], 400);
+		}
+
+		return $this->app->json($container->toArray());
 	}
 
 }
