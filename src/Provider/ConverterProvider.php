6c3ce93e3d83d4385da7cb972ce0ff3b8dbfe581
Chris Saylor <cjsaylor@gmail.com>
2014-08-01 08:12:35 -0400

    Implemented a converter provider.

---
+++ src/Provider/ConverterProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Boxmeup\Web\Provider;
+
+use Silex\ServiceProviderInterface;
+use Silex\Application;
+
+class ConverterProvider implements ServiceProviderInterface
+{
+	const PREFIX = 'converter.';
+
+	public function register(Application $app) {
+	}
+
+	public function boot(Application $app) {
+		foreach ($app['converter.converters'] as $converter) {
+			$app[static::PREFIX . $converter['prefix']] = $app->share(function() use ($app, $converter) {
+				return new $converter['class']($app['repo.' . $converter['repo']]);
+			});
+		}
+	}
+}
