ae4ca3c85c6ac7336cfe84c6232032f361389bed
Chris Saylor <cjsaylor@gmail.com>
2014-08-03 19:53:48 -0400

    Use jquery wrapper instead of angular element.
    This is possibly a concurrency issue when the JS is built.

--- web/js/Directive/bmuNavbarCollapse.js
+++ web/js/Directive/bmuNavbarCollapse.js
@@ -1,9 +1,9 @@
-define(['angular'], function(angular) {
+define(['jquery'], function($) {
 
 	return ['$window', function($window) {
 		return {
 			link: function(scope, el) {
-				var breakPoint = 991, $$window = angular.element($window);
+				var breakPoint = 991, $$window = $($window);
 				scope.navCollapse = true;
 				scope.collapse = function() {
 					if ($$window.width() > breakPoint) {
