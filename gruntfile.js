ce8a73734a7d3bc4a4fcc6652bf56225214a3161
Chris Saylor <cjsaylor@gmail.com>
2014-08-13 20:51:23 -0400

    Add travis support.
    For the time being, it will only run jshint until phpunit tests are
    added.

--- gruntfile.js
+++ gruntfile.js
@@ -48,4 +48,5 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('default', ['jshint']);
 	grunt.registerTask('build', ['compass', 'requirejs', 'releaseStamp']);
+	grunt.registerTask('travis', ['jshint']);
 };
