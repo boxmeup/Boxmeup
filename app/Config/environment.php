57c40182154ce1579f5e48c3e80b423d27aadd99
Chris Saylor <cjsaylor@gmail.com>
2015-10-04 23:01:45 -0400

    Make environment file perminent.
    Use environment variables for configuration.

---
+++ app/Config/environment.php
@@ -0,0 +1,40 @@
+<?php
+
+$config = array(
+	'Env' => array(
+		'debug' => getenv('DEBUG'),
+		'salt' => getenv('SALT'),
+		'cipher' => getenv('CIPHER'),
+		'Db' => array(
+			'default' => array(
+				'persistent' => false,
+				'host' => getenv('MYSQL_HOST_URL'),
+				'login' => getenv('MYSQL_USER'),
+				'password' => getenv('MYSQL_PASSWORD'),
+				'database' => getenv('MYSQL_DATABASE')
+			)
+		),
+		'Sphinx' => array(
+			'server' => getenv('SPHINX_HOST_URL') ?: 'sphinx',
+			'port' => getenv('SPHINX_PORT') ?: 9312
+		),
+		'Email' => array(
+			'mailgun' => array(
+				'domain' => getenv('MAILGUN_DOMAIN'),
+				'api_key' => getenv('MAILGUN_API_KEY')
+			)
+		),
+		'Feature' => array(
+			'api' => false,
+			'mobile' => true,
+			'analytics' => false,
+			'feedback' => false,
+			'user_registration' => true,
+			'bulk_export' => true,
+			'forgot_password' => true,
+			'autocomplete' => true,
+			'https_redirect' => false,
+			'beta' => getenv('BETA')
+		)
+	)
+);
