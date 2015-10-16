7e7ad4dce4f39a7b6c5a9f212593a7c60e4f50bc
Chris Saylor <cjsaylor@gmail.com>
2015-10-15 22:56:34 -0400

    Remove compass gem install from deploy step.

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -4,7 +4,6 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     npm i -g bower
     bower install
-    gem install compass
     ./node_modules/.bin/grunt build
     docker build -q -t cjsaylor/boxmeup .
     docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
